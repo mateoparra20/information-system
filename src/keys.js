d817f6feeadcd45ebe66d9a75f0097804ec5e8d3
mateo <mateoparra@usantotomas.edu.co>
2022-04-07 21:36:09 -0500

    first fix in the app

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,8 @@
 module.exports = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        database: 'database_information_system'
+        host: 'us-cdbr-east-05.cleardb.net',
+        user: 'b041adab13d426',
+        password: '1c9a23a3',
+        database: 'heroku_a72350352312ba8'
     }
 }
