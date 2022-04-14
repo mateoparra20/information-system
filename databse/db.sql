d7ea829c9fd3ed8b75c9e9be0643d7efc95b3898
SergioLagos <lagossergio0@gmail.com>
2022-04-14 16:08:09 -0500

    Correción BD creación correct; drop all

--- databse/db.sql
+++ databse/db.sql
@@ -1,10 +1,14 @@
+  
 use heroku_a72350352312ba8;
+
+SET @@auto_increment_increment=1;
 CREATE TABLE roles (
    role_id   BIGINT NOT NULL AUTO_INCREMENT,
    role VARCHAR(20) NOT NULL,
   PRIMARY KEY (role_id)
 );
 
+SET @@auto_increment_increment=1;
 CREATE TABLE users (
    user_id   BIGINT NOT NULL AUTO_INCREMENT,
    email  VARCHAR(100) NOT NULL,
@@ -20,12 +24,14 @@ CREATE TABLE users (
   CONSTRAINT fk_role_users FOREIGN KEY(role_id) REFERENCES roles(role_id)
 );
 
+SET @@auto_increment_increment=1;
 CREATE TABLE vital_signs (
    vs_id   BIGINT NOT NULL AUTO_INCREMENT,
    vital_sign  VARCHAR(20) NOT NULL,
   PRIMARY KEY (vs_id)
 );
 
+SET @@auto_increment_increment=1;
 CREATE TABLE anomalies (
    anomaly_id  int(100) NOT NULL AUTO_INCREMENT,
    vs_id  BIGINT NOT NULL,
@@ -38,6 +44,7 @@ CREATE TABLE anomalies (
   CONSTRAINT fk_vs_anom FOREIGN KEY(vs_id) REFERENCES vital_signs(vs_id)
 );
 
+SET @@auto_increment_increment=1;
 CREATE TABLE  alert_history  (
    alert_hist_id  int NOT NULL AUTO_INCREMENT,
    time_record  datetime NOT NULL,
@@ -48,6 +55,7 @@ CREATE TABLE  alert_history  (
   CONSTRAINT fk_anom_hist FOREIGN KEY(anomaly_id) REFERENCES anomalies(anomaly_id)
 );
 
+SET @@auto_increment_increment=1;
 CREATE TABLE  vital_signs_users  (
    vs_user_id   BIGINT NOT NULL AUTO_INCREMENT,
    user_id  BIGINT NOT NULL,
@@ -59,9 +67,21 @@ CREATE TABLE  vital_signs_users  (
    CONSTRAINT fk_vs_vsu FOREIGN KEY(vs_id) REFERENCES vital_signs(vs_id)
 );
 
+-- Roles:
 insert into roles(role) values ('user');
-insert into roles(role) values ('analist') ;
-
+insert into roles(role) values ('analist');
+-- Signos vitales:
 insert into vital_signs(vital_sign) values ('Frecuencia Cardíaca');
 insert into vital_signs(vital_sign) values ('Oximetría') ;
 insert into vital_signs(vital_sign) values ('Temperatura') ;
+
+SET @@auto_increment_increment=1;
+
+-- Drop all:
+-- use heroku_a72350352312ba8;
+-- Drop table vital_signs_users;
+-- Drop table alert_history;
+-- Drop table anomalies;
+-- Drop table users;
+-- Drop table vital_signs;
+-- Drop table roles;
