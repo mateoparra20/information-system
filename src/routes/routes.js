29b493bcf58228899fa22d0c14597aaf01cc7ff2
SergioLagos <lagossergio0@gmail.com>
2022-04-14 01:18:01 -0500

    insertar datos a la tabla vital_signs_users para test

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -18,11 +18,38 @@ router.get('/user-data', isLoggedIn, (req, res) => {
 });
 
 router.get('/analist-data', isLoggedIn, async (req, res) => {
-    const vitalSign = await pool.query('SELECT vital_sign FROM vital_signs');
+    const vitalSign = await pool.query('SELECT vs_id FROM vital_signs');
     console.log(vitalSign[0]);
     const users = await pool.query('SELECT u.user_id, u.identification, u.name, u.lastname FROM users u JOIN roles r ON u.role_id = r.role_id WHERE r.role = "user";');
-    console.log(users);
-    console.log(moment().format('YYYY-MM-DD HH:mm:ss'));
+    // console.log(users);
+    // console.log(moment().format('YYYY-MM-DD HH:mm:ss'));
+    const new_vs_u = {
+        user_id : null,
+        vs_id: null,
+        measure: null,
+        time_record: null
+    };
+
+    for (let step = 0; step < 24; step++) {
+        var u = Math.floor(Math.random() * users.length);
+        var L = Math.floor(Math.random() * 3);
+        var M = Math.floor(Math.random() * 100);
+        
+        
+        // console.log(users[u].user_id);
+        // console.log(vitalSign[L].vs_id);
+        new_vs_u.user_id = users[u].user_id;
+        new_vs_u.vs_id = vitalSign[L].vs_id;
+        new_vs_u.measure = parseFloat(M);
+        new_vs_u.time_record = moment().format('YYYY-MM-DD HH:mm:ss');
+        const result_sv_u = await pool.query('INSERT INTO vital_signs_users SET ?', [new_vs_u]);
+        // setTimeout(() => {
+        //     console.log("1 Segundo esperado")
+        //   }, 1000);
+        // new_vs_u.vs_user_id = result_sv_u.insertId;
+        console.log(result_sv_u);
+    }
+
     res.render('analist-data-view/analist-data', { users });
 });
 
