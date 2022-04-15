c411b62e3876a0120775cb2c4725d0da92320276
mateo <mateoparra@usantotomas.edu.co>
2022-04-14 20:08:56 -0500

    Comentar el for para que no meta más registros por ahora

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -21,34 +21,6 @@ router.get('/analist-data', isLoggedIn, async (req, res) => {
     const vitalSign = await pool.query('SELECT vs_id FROM vital_signs');
     console.log(vitalSign[0]);
     const users = await pool.query('SELECT u.user_id, u.identification, u.name, u.lastname FROM users u JOIN roles r ON u.role_id = r.role_id WHERE r.role = "user";');
-    // console.log(users);
-    // console.log(moment().format('YYYY-MM-DD HH:mm:ss'));
-    const new_vs_u = {
-        user_id : null,
-        vs_id: null,
-        measure: null,
-        time_record: null
-    };
-
-    for (let step = 0; step < 50; step++) {
-        var u = Math.floor(Math.random() * users.length);
-        var L = Math.floor(Math.random() * 3);
-        var M = Math.floor(Math.random() * 100);
-        
-        
-        // console.log(users[u].user_id);
-        // console.log(vitalSign[L].vs_id);
-        new_vs_u.user_id = users[u].user_id;
-        new_vs_u.vs_id = vitalSign[L].vs_id;
-        new_vs_u.measure = parseFloat(M);
-        new_vs_u.time_record = moment().format('YYYY-MM-DD HH:mm:ss');
-        const result_sv_u = await pool.query('INSERT INTO vital_signs_users SET ?', [new_vs_u]);
-        // setTimeout(() => {
-        //     console.log("1 Segundo esperado")
-        //   }, 2000);
-        // console.log(result_sv_u);
-    }
-
     res.render('analist-data-view/analist-data', { users });
 });
 
@@ -78,3 +50,33 @@ router.post('/analist-data', (req, res) => {
 });
 
 module.exports = router
+
+
+/**
+ * LO DEJE ACA PORQUE A MI NO ME APARECE LA OPCIÓN DE MINIMIZAR XD
+// console.log(users);
+// console.log(moment().format('YYYY-MM-DD HH:mm:ss'));
+// const new_vs_u = {
+//     user_id : null,
+//     vs_id: null,
+//     measure: null,
+//     time_record: null
+// };
+
+// for (let step = 0; step < 50; step++) {
+//     var u = Math.floor(Math.random() * users.length);
+//     var L = Math.floor(Math.random() * 3);
+//     var M = Math.floor(Math.random() * 100);
+//     // console.log(users[u].user_id);
+//     // console.log(vitalSign[L].vs_id);
+//     new_vs_u.user_id = users[u].user_id;
+//     new_vs_u.vs_id = vitalSign[L].vs_id;
+//     new_vs_u.measure = parseFloat(M);
+//     new_vs_u.time_record = moment().format('YYYY-MM-DD HH:mm:ss');
+//     const result_sv_u = await pool.query('INSERT INTO vital_signs_users SET ?', [new_vs_u]);
+    // setTimeout(() => {
+    //     console.log("1 Segundo esperado")
+    //   }, 2000);
+    // console.log(result_sv_u);
+// }
+*/
