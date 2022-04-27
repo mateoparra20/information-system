5e815586d09af9dcf8e610f662995e0fe1b6a42c
SergioLagos <lagossergio0@gmail.com>
2022-04-26 23:50:28 -0500

    Alertas

--- src/lib/vital-signal-db.js
+++ src/lib/vital-signal-db.js
@@ -1,3 +1,6 @@
+require('dotenv');
+const client = require('twilio')("AC24d6cbd97e4536d4b5b1e797752ff69c", "501195b094519e04625db6eb422861c4");
+
 const pool = require('../database');
 const moment = require('moment');
 
@@ -6,14 +9,48 @@ modelSignalDb = {
     vs_id: null,
     measure: null,
     time_record: null
+};
+
+modelAlertDb = {
+    user_id : null,
+    anomaly_id: null,
+    time_record: null
 }
 const vitalSignalDb = {
     registrar: async function( modelSignalDb ){
-        moment.locale('de')
-        console.log(moment().format('YYYY-MM-DD HH:mm:ss:SSS'))
+        
+        let anomaliesQuery = null;
+        moment.locale('de');
         modelSignalDb.time_record = moment().format('YYYY-MM-DD HH:mm:ss:SSS');
         await pool.query('SET @@auto_increment_increment=1');
         await pool.query('INSERT INTO vital_signs_users SET ?', [modelSignalDb]);
+        
+        anomaliesQuery = await pool.query('SELECT anomaly_id, description FROM anomalies WHERE vs_id = ? AND max_rate > ? AND min_rate < ?',[modelSignalDb.vs_id,modelSignalDb.measure,modelSignalDb.measure] );
+
+        if (anomaliesQuery[0] !== undefined){
+            let analist = await pool.query('SELECT phone, name FROM users WHERE role_id="2"');
+            let user = await pool.query('SELECT phone, name FROM users WHERE user_id = ?', [modelSignalDb.user_id]);
+
+            analist.forEach(element => {
+                client.messages.create({
+                    from: 'whatsapp:+14155238886',
+                    body: 'Hola analista ' + element.name +', según nuestro reporte, el usuario ' + user[0].name + ' presenta la anomalía '+ anomaliesQuery[0].description +' según sus signos vitales, ponte en contacto con el usuario inmediatamente',
+                    to: 'whatsapp:+57' + element.phone,
+                }).then(message => console.log(message.sid));
+            });
+            
+            client.messages.create({
+                from: 'whatsapp:+14155238886',
+                body: 'Hola usuario ' + user[0].name + ', según nuestro reporte, presentas la anomalía '+ anomaliesQuery[0].description +' según tus signos vitales, ponte en contacto con cualquier analista inmediatamente',
+                to: 'whatsapp:+57' + user[0].phone,
+            }).then(message => console.log(message.sid));
+
+            modelAlertDb.time_record = modelSignalDb.time_record;
+            modelAlertDb.user_id = modelSignalDb.user_id;
+            modelAlertDb.anomaly_id = anomaliesQuery[0].anomaly_id;
+            await pool.query('SET @@auto_increment_increment=1');
+            await pool.query('INSERT INTO alert_history SET ?', [modelAlertDb]);
+        }
     }
 }
 
--- src/routes/vital-signal.js
+++ src/routes/vital-signal.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const model = require('../lib/vital-signal-db');
 
+require('dotenv').config();
+require('twilio')("AC24d6cbd97e4536d4b5b1e797752ff69c", "501195b094519e04625db6eb422861c4");
+
 const router = express.Router();
 
 router.get('/information-data', (req, res) => {
